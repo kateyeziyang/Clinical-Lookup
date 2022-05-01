16d8aa22d1db121518124c71351cc46bed00a63a
Jiaming Zhang <qq1104382148@gmail.com>
2022-05-01 01:12:13 -0500

    rough web worked

--- website/src/Home.js
+++ website/src/Home.js
@@ -6,13 +6,31 @@ import './Home.css';
 const Home = () => {
 
   const [topic, setTopic] = useState("");
-  const [doc, setDoc] = useState([]);
+  const [doc, setDoc] = useState();
+  const [data, setData] = useState({})
   const [url, setUrl] = useState("");
+
+  useEffect(() => {
+    fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data)
+    }).then(response => {
+      if (response.ok) {
+        return response.json()
+      }
+    }).then(data => {
+      console.log(data)
+      setDoc(data);
+    })
+  }, [url, data])
 
   return (
     <>
       <div className='HomeContainer'>
-        <Input topic={topic} setTopic={setTopic} setUrl={setUrl}/>
+        <Input topic={topic} setTopic={setTopic} setUrl={setUrl} setData={setData}/>
       </div>
       <div className='HomeContainer'>
         <Output doc={doc} />
--- website/src/Input.js
+++ website/src/Input.js
@@ -1,17 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
+import NativeSelect from '@mui/material/NativeSelect';
 import './Home.css'
 
 
 
 const Input = (props) => {
-  const {topic, setTopic, setUrl} = props;
+  const {topic, setTopic, setUrl, setData} = props;
+  const [mode, setMode] = useState("bm25");
+
+  const handleSwitch = (e) => {
+    setMode(e.target.value);
+  }
 
   const handleSubmit = (e) => {
-    let url = "" + topic;
-    setUrl(url);
+    e.preventDefault();
+    let temp_url = "http://127.0.0.1:5000/" + mode;
+    setUrl(temp_url);
+    let temp_data = {"topic":topic};
+    setData(temp_data);
   }
+
+
 
   const handleChange = (e) => {
     setTopic(e.target.value)
@@ -20,6 +31,12 @@ const Input = (props) => {
   return (
     <form onSubmit={handleSubmit}>
       <TextField id="standard-basic" label="Topics" variant="standard" onChange={handleChange}/>
+      <div className='Switch'>
+        <NativeSelect defaultValue='bm25' onChange={handleSwitch}>
+          <option value='bm25'>BM_25</option>
+          <option value='keybert'>KeyBert</option>
+        </NativeSelect>
+      </div>
       <div className='SubmitButton'>
         <Button type='submit' variant='outlined'>
           Submit
